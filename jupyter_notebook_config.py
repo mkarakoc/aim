798824e3007ab616ba7b43b2e7889119aa0de39f
Mesut Karakoç <mkarakoc@users.noreply.github.com>
2018-02-04 11:38:12 +0300

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -183,7 +183,7 @@
 # c.NotebookApp.mathjax_url = ''
 
 # The directory to use for notebooks and kernels.
-# c.NotebookApp.notebook_dir = u''
+c.NotebookApp.notebook_dir = u'/home/main/aim/'
 
 # Whether to open in a browser after starting. The specific browser used is
 # platform dependent and determined by the python standard library `webbrowser`
