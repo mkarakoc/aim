bed61d050a1de1558a28d9bdce682551d1e1fa28
mkarakoc <mesudkarakoc@hotmail.com>
2020-02-23 18:29:54 +0300

    python3

---
+++ examples/runall.sh
@@ -0,0 +1,8 @@
+
+jupyter nbconvert --to notebook --execute --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=-1 00_AIMpy_yukawa_potential.ipynb --output 00_AIMpy_yukawa_potential_output.ipynb
+ 
+jupyter nbconvert --to notebook --execute --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=-1 01_AIMpy_exp_cos_screened_coulomb_potential.ipynb --output 01_AIMpy_exp_cos_screened_coulomb_potential_output.ipynb
+ 
+jupyter nbconvert --to notebook --execute --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=-1 02_AIMpy_DKP_sextic_osc.ipynb --output 02_AIMpy_DKP_sextic_osc_output.ipynb
+ 
+jupyter nbconvert --to notebook --execute --ExecutePreprocessor.enabled=True --ExecutePreprocessor.timeout=-1 03_AIMpy_black_hole.ipynb --output 03_AIMpy_black_hole_output.ipynb
